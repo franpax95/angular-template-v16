b6bd2ef49e8b3f98fd840588eb33d2717a98be87
Franpax <fcojnavarrowork@gmail.com>
2023-05-10 19:32:23 +0200

    Config prettier & eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-template-v16';
+export class AppComponent implements OnInit {
+	public title: string = 'angular-template-v16';
+
+	public arr: Array<number> = [1, 2];
+
+	public Juan: string = 'Fran';
+
+	/** Comment */
+	public comment: string = 'Comments';
+
+	constructor() {}
+
+	public ngOnInit(): void {
+		throw new Error('Method not implemented.');
+	}
+
+	public method1(): void {
+		console.dir('method1');
+	}
+
+	public method2(): void {
+		console.dir('method2');
+		console.dir('');
+		console.dir('Juan');
+		const ok: boolean | null = true;
+		const Juan: string = 'juan';
+
+		const array: Array<number> = [1, 2];
+		const obj: any = { Juan, array: { Juan } };
+	}
 }
